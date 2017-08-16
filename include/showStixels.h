8e063330020aece965649c2fbdbcf88922359c4a
Unknown <zhangsongyi@stu.xjtu.edu.cn>
2017-08-16 11:38:25 +0800

    fix the slash problem

--- include/showStixels.h
+++ include/showStixels.h
@@ -1,7 +1,7 @@
 #ifndef SHOW_STIXELS_H_
 #define SHOW_STIXELS_H_
 
-#include <opencv2\core.hpp>
+#include <opencv2/core.hpp>
 #include "struct.h"
 
 void ShowStixels(cv::Mat left_frame, cv::Mat& left_frame_stx, Section* stx, StixelModelParameters model_parameters, int real_cols, int horizon_point, int max_dis_display);
